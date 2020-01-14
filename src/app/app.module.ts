5587cdd150cce47503d168c82b50d394114b4526
Oscar PHILIPPOT <oscar.philippot@hei.yncrea.fr>
2020-01-14 10:41:12 +0100

    categorie page web et correction app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CinemaComponent } from './cinema/cinema.component';
@@ -26,7 +28,9 @@ import { FilmComponent } from './film/film.component';
     FilmComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
